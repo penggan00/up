2db2e73085d8d120e131263510551fc5c289ee44
好酒 <106035465+penggan00@users.noreply.github.com>
2024-08-14 00:35:31 +0800

    2024.8.14

--- workflows/pp.js
+++ workflows/pp.js
@@ -1,33 +1,10 @@
-addEventListener('scheduled', event => {
-  event.waitUntil(handleScheduled(event));
-});
+export default {
+  async fetch(request) {
+    /**
+     * Replace `remote` with the host you wish to send requests to
+     */
+    const remote = "https://epg.112114.xyz/pp.xml";
 
-async function handleScheduled(event) {
-  const url = 'https://epg.112114.xyz/pp.xml';
-  
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const content = await response.text();
-    
-    // Store the content in KV storage
-    await EPG_STORAGE.put('pp.xml', content);
-
-    console.log('File updated successfully');
-  } catch (error) {
-    console.error('Failed to update file:', error);
-  }
-}
-
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
-  const content = await EPG_STORAGE.get('pp.xml');
-  return new Response(content, {
-    headers: { 'Content-Type': 'application/xml' },
-  });
-}
+    return await fetch(remote, request);
+  },
+};
