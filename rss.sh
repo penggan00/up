deb729c61ac83dd02cf7c1c1dcf8557fcbeba55b
好酒 <106035465+penggan00@users.noreply.github.com>
2024-10-24 23:10:10 +0800

    更新 rss.sh

--- rss.sh
+++ rss.sh
@@ -26,11 +26,76 @@ source rss_venv/bin/activate
 echo "安装必要的库..."
 pip install --upgrade pip
 pip install aiohttp aiomysql feedparser python-dotenv python-telegram-bot
+pip install requests html2text python-telegram-bot beautifulsoup4 langdetect markdownify mysql-connector-python pytz
 
 # 创建 rss.py 文件（如果不存在）
 if [ ! -f "rss.py" ]; then
     echo "创建 rss.py 文件..."
     touch rss.py
 fi
+# 创建 mail.py 文件（如果不存在）
+if [ ! -f "mail.py" ]; then
+    echo "创建 mail.py 文件..."
+    touch mail.py
+fi
+# 创建 rss2.py 文件（如果不存在）
+if [ ! -f "rss2.py" ]; then
+    echo "创建 rss2.py 文件..."
+    touch rss2.py
+fi
+
+# 创建 rss.sh 脚本
+if [ ! -f "~/rss.sh" ]; then
+    echo "创建 rss.sh 文件..."
+    cat << 'EOF' > ~/rss.sh
+#!/bin/bash
+
+# 检查 rss.sh 是否在运行
+if pgrep -f "rss.sh" > /dev/null; then
+    echo "rss.sh is running. Stopping it..."
+    # 停止 rss.sh 进程
+    pkill -f "rss.sh"
+else
+    echo "rss.sh is not running."
+fi
+# 等待2秒
+sleep 1
+# 检查 mail.py 是否在运行
+if pgrep -f "mail.py" > /dev/null; then
+    echo "mail.py is running. Stopping it..."
+    # 停止 mail.py 进程
+    pkill -f "mail.py"
+else
+    echo "mail.py is not running."
+fi
+# 等待2秒
+sleep 1
+# 检查 rss.py 是否在运行
+if pgrep -f "rss.py" > /dev/null; then
+    echo "rss.py is running. Stopping it..."
+    # 停止 rss.py 进程
+    pkill -f "rss.py"
+else
+    echo "rss.py is not running."
+fi
+# 等待2秒
+sleep 1
+# 运行 RSS 脚本
+source ~/rss/rss_venv/bin/activate
+python3 ~/rss/rss.py
+deactivate
+
+sleep 1
+# 运行 RSS 脚本
+source ~/mail/mail_venv/bin/activate
+python3 ~/mail/mail.py
+deactivate
+EOF
+    chmod +x ~/rss.sh
+fi
+
+# 为 cron 添加任务
+echo "添加 crontab 任务..."
+(crontab -l 2>/dev/null; echo "30 * * * * ~/rss.sh") | crontab -
 
 echo "项目设置完成！"
